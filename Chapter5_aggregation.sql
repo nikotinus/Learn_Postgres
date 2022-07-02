19f4957a2854ffa1f274240499fefa75846eb93d
alex ser <serkin2005@mail.ru>
2022-07-03 01:07:21 +0300

    done with chapter 4

--- Chapter5_aggregation.sql
+++ Chapter5_aggregation.sql
@@ -1,8 +1,8 @@
 select 
-	min(pp.price) min_price
-	, max(pp.price) max_price
-	, count(*) count_rows
-from product_price pp
+	min(pp.price) as min_price
+	, max(pp.price) as max_price
+	, count(*) as count_rows
+from product_price as pp
 where pp.price < 0;
 
 -- 5_13
@@ -23,7 +23,7 @@ from product_price pp
 join product p 
 on p.product_id = pp.product_id 
 group by p.name 
-order by 1
+order by 1;
 
 --7_13
 select 
@@ -76,6 +76,103 @@ on m.employee_id = e.manager_id
 group by 1,2
 order by 1,2;
 
-select e.first_name & e.last_name from employee e 
-
 --10/13
+/*
+Для каждого товара получи минимальную и максимальную стоимость из таблицы product_price. 
+Выведи столбцы:
+
+product_id - идентификатор товара;
+price_min - минимальная стоимость товара;
+price_max - максимальная стоимость товара.
+В результате оставь только те товары, для которых минимальная и максимальная стоимость отличается.
+
+Отсортируй результат по идентификатору товара.
+ */
+select
+	pp.product_id 
+	, min(pp.price) as price_min
+	, max(pp.price) as price_max
+from product_price as pp
+group by pp.product_id 
+having min(pp.price) != max(pp.price)
+order by 1;
+
+-- roolup 11/13
+/*
+Получи информацию о количестве сотрудников из таблицы employee:
+
+в каждом магазине на каждой должности;
+общее количество сотрудников в магазине;
+общее количество сотрудников во всех магазинах.
+В результате выведи столбцы:
+
+store_id - идентификатор магазина;
+rank_id - идентификатор должности;
+count_employees - количество сотрудников.
+Отсортируй результат сначала по идентификатору магазина, затем по идентификатору должности.
+Для обоих полей NULL значения размести в конце, воспользовавшись конструкцией NULLS LAST, 
+как это сделано в теории задания.
+*/
+select 
+	e.store_id 
+	, e.rank_id 
+	, count(*) as count_employees
+from employee as e
+group by rollup (e.store_id, e.rank_id) 
+order by 
+	1 nulls last
+	, 2 nulls last;
+
+--CUBE (12/13)
+/*
+Получи информацию о количестве сотрудников из таблицы employee:
+
+в каждом магазине на каждой должности;
+общее количество сотрудников в магазине;
+общее количество сотрудников, занимающих определенную должность;
+общее количество сотрудников во всех магазинах.
+В результате выведи столбцы:
+
+store_id - идентификатор магазина;
+rank_id - идентификатор должности;
+count_employees - количество сотрудников.
+Отсортируй результат сначала по идентификатору магазина, затем по идентификатору должности. 
+Для обоих полей NULL значения размести в конце, воспользовавшись конструкцией NULLS LAST, 
+как это сделано в теории задания.
+*/
+select 
+	e.store_id 
+	, e.rank_id 
+	, count(*) as count_employees
+from employee as e
+group by cube (e.store_id, e.rank_id) 
+order by 
+	1 nulls last
+	, 2 nulls last;
+	
+--
+/*
+GROUPING SETS (13/13)
+
+Получи информацию о количестве сотрудников из таблицы employee:
+
+в каждом магазине на каждой должности;
+общее количество сотрудников во всех магазинах.
+В результате выведи столбцы:
+
+store_id - идентификатор магазина;
+rank_id - идентификатор должности;
+count_employees - количество сотрудников.
+Отсортируй результат сначала по идентификатору магазина, 
+затем по идентификатору должности. Для обоих полей NULL 
+значения размести в конце, воспользовавшись конструкцией NULLS LAST, 
+как это сделано в теории задания.*/
+select 
+	e.store_id 
+	, e.rank_id 
+	, count(*) as count_employees
+from employee as e
+group by grouping sets ((e.store_id, rank_id), ()) 
+order by 
+	1 nulls last
+	, 2 nulls last;
